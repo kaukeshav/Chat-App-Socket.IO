05c38a74ee1952a5ba6eadb9116876935f1c5551
Keshav Kaushik <keshav.k@tadigital.com>
2019-04-01 23:08:19 +0530

    initial connection socket io client and server

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
-var port = 8080;
+var port = 8000;
 
 app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function(socket) {
+  console.log("new connection has been made");
+
+  socket.emit("message-from-server", {
+    greeting: "Hello from Server"
+  });
+
+  socket.on("message-from-client", function(msg) {
+    console.log(msg);
+  });
+});
 
 server.listen(port, function() {
   console.log("listening to Port no." + port);
